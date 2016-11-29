5ee5dfc6e0356a734087026b94597c4c7260141c
Serg Red <sergred@gmail.com>
2016-11-29 19:13:12 +0100

    Added code for the second assignment

---
+++ spark/assignment.2/_template_/part1/weather.scala
@@ -0,0 +1,85 @@
+// ================ PRE-PROCESSING CODE =================
+// Anything necessary to get the answers later.
+
+// val sc = new SparkContext()
+
+case class Weather(val date: String, val lat: Int, val lon: Int, val elev: Int, val wind: Int, val squal: String, val temp: Int, val tqual: String, val pres: Int, val pqual: String)
+
+def parse(line: String): Weather = {
+  val date = line.substring(15, 23)
+  val lat = line.substring(28, 34).toInt
+  val lon = line.substring(34, 41).toInt
+  val elev = line.substring(46, 51).toInt
+  val wind = line.substring(65, 69).toInt
+  val squal = line.substring(69, 70)
+  // val temp = line.substring(87, 92).toDouble/10
+  val temp = line.substring(87, 92).toInt
+  val tqual = line.substring(92, 93)
+  val pres = line.substring(99, 104).toInt
+  val pqual = line.substring(104, 105)
+  Weather(date, lat, lon, elev, wind, squal, temp, tqual, pres, pqual)
+}
+
+// val rdd = sc.textFile("projects/advprogramming/bigdata/spark/assignment.2/data/weather/2012/").map(parse(_))
+
+val rdd = sc.textFile("/home/big-data/datasets/weather/2012/").map(parse(_))
+// rdd.first
+
+val sweden = rdd.filter(_.lat != 99999).filter(_.lon != 999999).filter(_.elev != 9999).filter(_.wind != 9999).filter(_.temp != 9999).filter(_.pres != 99999).filter(_.squal == "1").filter(_.tqual == "1").filter(_.pqual == "1").filter(_.lat > 55382).filter(_.lat < 69047).filter(_.lon > 11391).filter(_.lon < 24034)
+
+// import org.apache.spark.storage.StorageLevel._
+// rdd.persist(MEMORY_AND_DISK)
+// sweden.persist(MEMORY_AND_DISK)
+
+sweden.persist()
+
+/** QUESTION 1
+* How many measures (records) are there?
+*
+* Answer: 1514332
+*/
+
+val count = sweden.count()
+// count: Long = 1514332
+
+/** QUESTION 2
+* Which is the highest elevation? Give latitude and longitude.
+* Answer:
+*
+* - Elevetaion = 1146
+* - Latitude = 67,917
+* - Longitude = 18,600
+*/
+
+val maxelev = sweden.reduce((a, b) => if (a.elev > b.elev) a else b)
+// maxelev: Weather = Weather(20121213,67917,18600,1146,20,1,-124,1,10215,1)
+maxelev.elev
+// res1: Int = 1146
+maxelev.lat
+// res2: Int = 67917
+maxelev.lon
+// res3: Int = 18600
+
+/** QUESTION 3
+* Which is the average wind speed in the country?
+*
+* Answer: 37.76
+*/
+
+val avgwind = sweden.map(_.wind).mean
+// avgwind: Double = 37.75939292044281
+
+/** QUESTION 4
+* Which is the average temperature per month? Sort from heights to lowest.
+*
+* Answer:
+* - January = ???
+* - February = ???
+* - March = ???
+* ...
+*/
+
+val avgtemp = sweden.groupBy(_.date.substring(4, 6).toInt).map(r => (r._1, r._2.map(_.temp).reduce(_ + _).toDouble/(10*r._2.size))).sortBy(_._2, false).take(12)
+// avgtemp: Array[(Int, Double)] = Array((7,14.879008141329217), (8,14.116240164998855), (6,11.319174477736237), (9,9.910506724485618), (5,8.090628200469126), (10,4.469014933270621), (11,2.2523962374530453), (4,2.1900031114895846), (3,1.4266762959396566), (1,-3.8700073608515937), (2,-5.66338860838438), (12,-6.23192790618755))
+
+sweden.toDf().write.parquet("2010-parquet")
---
+++ spark/assignment.2/_template_/part2/reddit/src/main/scala/Comments.scala
@@ -0,0 +1,111 @@
+/**
+* University of Skövde | Big data programming | Assignment 2: Spark
+*
+* HOW TO DEPLOY:
+* http://spark.apache.org/docs/latest/quick-start.html#self-contained-applications
+*/
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+
+/** SQL FUNCTIONS FOR DATA FRAMES e.g. avg("..."), orderBy(desc("...")).
+* http://spark.apache.org/docs/latest/api/scala/index.html#org.apache.spark.sql.functions$ */
+import org.apache.spark.sql.functions._
+
+
+object Comments {
+	def main(arg: Array[String]) = {
+		/** SPARK CONFIGURATION
+		* http://spark.apache.org/docs/latest/configuration.html
+		*
+		* The app name is shown in the Spark web UI: localhost:4040
+		 */
+		val conf = new SparkConf().setAppName("Reddit comments")
+
+
+		/** Create Spark context in case you're working with RDDs. */
+		val sc = new SparkContext(conf)
+		// You can silence the console output a bit...
+		// sc.setLogLevel("WARN")
+
+
+		/** Create Spark session for working with data frames. */
+		val spark = SparkSession.builder().getOrCreate()
+		// Needed import if you want to use $ for columns e.g. $"author"...
+		import spark.implicits._
+
+
+
+		// PATH TO DATA. Check if it is correct.
+		// val dataPath = "/home/big-data/datasets/reddit/2010"
+		// PATH TO STOP WORDS FILE.
+		// val swPath = "res/stopwords.txt"
+		val dataPath = "projects/advprogramming/bigdata/spark/assignment.2/data/reddit/2010"
+		val swPath = "projects/advprogramming/bigdata/spark/assignment.2/_template_/part2/reddit/res/stopwords.txt"
+
+		val df = spark.read.parquet(dataPath) // read all parquet files which are stored in dataPath directory
+		// df.printSchema
+		// df.show
+
+		// --------------------------------------------------------------
+		// TASK 1
+		// --------------------------------------------------------------
+
+		val hours = df.select(hour(from_unixtime($"created_utc")).as("hour")).groupBy($"hour").count.orderBy("hour")
+		// selected created_utc column
+		// converted created_utc value from string to date
+		// exstracted hour from date
+		// renamed it to "hour"
+		// grouped by hour
+		// counted it
+		hours.write.csv("commentcount")
+		// The results are written to commentcount.csv
+
+		// --------------------------------------------------------------
+		// TASK 2
+		// --------------------------------------------------------------
+
+		val ups = df.select($"subreddit", $"ups").groupBy("subreddit").agg(avg("ups").as("average")).orderBy(desc("average"))
+		// selected subreddit and ups columns
+		// grouped by subreddit
+		// applied avg aggregate function to ups column
+		// ordered by average
+		ups.write.csv("upsaverage")
+		// The results are written to upsaverage.csv
+
+		// --------------------------------------------------------------
+		// TASK 3
+		// --------------------------------------------------------------
+
+		val highest = df.select(weekofyear(from_unixtime($"created_utc")).as("week"), $"ups", $"body").orderBy(desc("ups")).groupBy($"week").agg(first("ups").as("ups"), first("body").as("comment")).orderBy($"week")
+		// selected created_utc and ups columns
+		// extracted a week number created_utc
+		// renamed it to "week"
+		// grouped by week
+		// applied max aggregate function to ups column
+		// join with df and select week, ups and body
+		// order by week
+		highest.write.parquet("weekheights")
+		// The results are written to weekheights parquet file
+
+		// --------------------------------------------------------------
+		// TASK 4
+		// --------------------------------------------------------------
+
+		val stop = sc.textFile(swPath).collect
+		val count = df.select($"body").explode("body", "word")((line: String) => line.replace(".", " ").replace("-", " ").replace(",", " ").split(" +").filter(!stop.contains(_))).groupBy("word").count.orderBy(desc("count")).limit(200)
+		// selected body column
+		// excluded ".", ",", "-" and all stop words from each comment
+		// exploded each comment - replaced each record by several records which contain one word out of the comment
+		// grouped by each word
+		// count rows in each group
+		// ordered by count
+		// limited by 200 lines
+		count.write.csv("wordcount")
+		// The results are written to wordcount csv file
+
+		// --------------------------------------------------------------
+		sc.stop
+	}
+}
