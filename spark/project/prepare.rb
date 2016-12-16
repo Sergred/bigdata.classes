2157aa3ebb056d88e3335089edf23e67b44a70a7
Serg Red <sergred@gmail.com>
2016-12-16 11:36:24 +0100

    Updated ruby parser

--- spark/project/prepare.rb
+++ spark/project/prepare.rb
@@ -1,19 +1,28 @@
 require 'nokogiri'
 
-for item in Dir['/home/serg/projects/advprogramming/bigdata/spark/project/www.bbc.com/news/*'] do
-  if item =~ /.*-\d+/ and not File.directory?(item)
-    doc = File.open(item) { |f| Nokogiri::HTML(f) }
-    # File.open(item, 'w') do |file|
-    puts doc.xpath("//h1[@class='story-body__h1']").first
-    puts doc.xpath("//li[@class='mini-info-list__item']/div/@data-datetime")
-    puts doc.xpath("//div[@class='story-body__inner' and @property='articleBody']/p")
-    puts
-      # file.write(doc.xpath("//h1[@class='story-body__h1']").first + "\n")
-      # file.write(doc.xpath("//li[@class='mini-info-list__item']/div/@data-datetime").first + "\n")
-      # cnt = doc.xpath("//div[@class='story-body__inner' and @property='articleBody']/p")
-      # file.write(cnt.inject(""){|sum, x| sum + x.content }) if not cnt.empty?
-    # end
-  else
-    # File.delete(item) if not File.directory?(item)
+path = '/home/serg/projects/advprogramming/pool/'
+output = '/home/serg/projects/advprogramming/data/'
+for dir in Dir.entries(path).select {|entry| File.directory? File.join(path, entry) and !(entry == '.' || entry == '..')}
+  # puts File.join(path, dir)
+  for item in Dir.glob("#{File.join(path, dir)}/**/*") do
+    if item =~ /.*-\d+/ and not File.directory?(item)
+      # puts item
+      # puts item.split('/')[-1].split('-')[0..-2].join(' ')
+      # puts item.split('/')[-1].split('-')[-1]
+      doc = File.open(item) { |f| Nokogiri::HTML(f) }
+      if !doc.xpath("//h1[@class='story-body__h1']").first.nil? && !doc.xpath("//li[@class='mini-info-list__item']/div/@data-datetime").first.nil? && !doc.xpath("//div[@class='story-body__inner' and @property='articleBody']/p").nil?
+        puts File.join(output, dir + "-" + item.split('/')[-1].split('-')[-1])
+        File.open(File.join(output, dir + "-" + item.split('/')[-1].split('-')[-1]), 'w') do |file|
+          file.write(dir + "%%")
+          file.write(item.split('/')[-1].split('-')[0..-2].join(' ') + "%%")
+          file.write(item.split('/')[-1].split('-')[-1] + "%%")
+          file.write(doc.xpath("//li[@class='mini-info-list__item']/div/@data-datetime").first.text + "%%")
+          file.write(doc.xpath("//h1[@class='story-body__h1']").first.text + "%%")
+          cnt = doc.xpath("//div[@class='story-body__inner' and @property='articleBody']/p")
+          file.write(cnt.inject(""){|sum, x| sum + x.content }) if not cnt.empty?
+        end
+      end
+    end
+    File.delete(item) if not File.directory? item
   end
 end
